dbd488e9dfed4a6440c8236bc7ffb5920eea2380
Patryk Byszek <byszek.patryk@gmail.com>
2021-06-03 14:12:58 +0200

    work in progress

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,12 @@
 import '../styles/app.css';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import Navigation from './Navigation';
 import Page from './Page';
 
 function App() {
   return (
     <div className="App">
-      <Router basename="/React">
+      <Router>
         <Navigation />
         <Page />
       </Router>
